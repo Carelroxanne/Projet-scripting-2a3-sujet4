29a0c56edddaaaaf9ae90e6d462b98af4f846f87
Carelroxanne <47859067+Carelroxanne@users.noreply.github.com>
2020-03-06 17:26:20 +0100

    Create lblk.sh

---
+++ lblk.sh
@@ -0,0 +1,11 @@
+# Projet-scripting-2a3-sujet4
+Projet realisé en langage shell dans le module de scripting
+#!/bin/sh
+
+yad \
+--width=500 \
+--height=500 \
+yad --text-info --back=cyan < lblk.txt
+
+./choix.sh
+
